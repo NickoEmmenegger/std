7392a27264cda750c4729ac180fb98b85e25c8a4
nicko.emmenegger <nicko.emmenegger@students.fhnw.ch>
2023-12-07 20:36:56 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 import pandas as pd
-from PIL import Image
 import streamlit as st
 import plotly.express as px
-from streamlit.components.v1 import html
-from ipyvizzu import Chart, Data, Config, Style, DisplayTarget
 
 # Function to create Plotly line plot for spending trends
 def plot_spending_trends(data, league):
@@ -30,13 +27,40 @@ def load_data():
     combined_df.rename(columns={'fee_cleaned': 'Transfer Fee in Millions', 'league_name': 'League Name', 'club_name': 'Club Name'}, inplace=True)
     return combined_df
 
+# Function to display images in a responsive layout
+def display_images_responsive(image_paths, captions):
+    if st.session_state.get('screen_width', 0) <= 640:  # Mobile devices
+        for image, caption in zip(image_paths, captions):
+            st.image(image, caption=caption, use_column_width=True)
+    else:  # Larger screens
+        cols = st.columns(3)
+        for col, image, caption in zip(cols, image_paths, captions):
+            col.image(image, caption=caption, width=200)
+
 # Load data
 combined_transfers = load_data()
 
-# URLs or paths to the images
-neymar_image = 'images/neymar_image.jpg'
-psg_logo = 'images/psg_logo.jpg'
-mbappe_image = 'images/mbappe_image.jpg'
+# App title
+st.title('The Financial Dynamics of European Football\'s Elite Leagues')
+
+# Check screen width to adjust layout
+screen_width = st.session_state.get('screen_width', 0)
+if screen_width == 0:
+    st.markdown("""
+        <script>
+        const updateScreenWidth = () => {
+            window.parent.postMessage({
+                type: 'streamlit:setComponentValue',
+                value: window.innerWidth
+            }, '*');
+        };
+        updateScreenWidth();
+        window.addEventListener('resize', updateScreenWidth);
+        </script>
+    """, unsafe_allow_html=True)
+
+if screen_width > 0:
+    st.session_state['screen_width'] = screen_width
 
 # App title
 st.title('The Financial Dynamics of European Football\'s Elite Leagues')
@@ -44,13 +68,13 @@ st.title('The Financial Dynamics of European Football\'s Elite Leagues')
 # Introduction to Football Transfer Market
 st.header("Introduction to the Football Transfer Market")
 st.markdown("""
-    The European football transfer market is a dynamic and evolving financial powerhouse, reflecting more than just the sport's competitive spirit. Historically, it has seen groundbreaking transfers that have redefined club strategies and economic approaches. Today, it stands as a testament to the evolving financial capabilities of clubs, shaped by sponsorships, media rights, and transformative ownership models. This narrative delves into the pulsating heart of this market, exploring the spending trends, player valuations, and the strategic financial maneuvers that define the competitive landscape of Europe's top football leagues.
+    The European football transfer market, covering data from the 1992/93 to 2021/22 seasons, presents a dynamic and evolving financial powerhouse, reflecting the sport's competitive spirit and economic evolution. This period has witnessed groundbreaking transfers that have redefined club strategies and economic approaches. Today, it stands as a testament to the evolving financial capabilities of clubs, shaped by sponsorships, media rights, and transformative ownership models. This narrative delves into the pulsating heart of this market, exploring the spending trends, player valuations, and the strategic financial maneuvers that define the competitive landscape of Europe's top football leagues.
 """, unsafe_allow_html=True)
 
 # League-Wise Spending Overview
 st.header("League-Wise Spending Overview")
 st.markdown("""
-    Dive into the financial dynamics of Europe's elite football leagues, where spending patterns reveal more than just numbers. This comparative analysis sheds light on the diverse financial strategies employed across leagues – from prioritizing youth development to splurging on marquee signings. Each league's approach, whether it's the Premier League's global branding prowess or La Liga's blend of star power and homegrown talents, tells a unique story of financial planning, club philosophies, and market-driven tactics.
+    Dive into the financial dynamics of Europe's elite football leagues, as seen through data spanning from 1992/93 to 2021/22. This comparative analysis sheds light on the diverse financial strategies employed across leagues – from prioritizing youth development to splurging on marquee signings. Each league's approach tells a unique story of financial planning, club philosophies, and market-driven tactics. The data reveals how spending patterns have evolved over nearly three decades, showcasing the economic growth and strategic shifts within these leagues.
 """)
 league_spending = combined_transfers[combined_transfers['transfer_movement'] == 'in'].groupby('League Name')['Transfer Fee in Millions'].sum().reset_index().sort_values(by='Transfer Fee in Millions', ascending=False)
 fig = px.bar(league_spending, x='League Name', y='Transfer Fee in Millions', title='Total Spending per League (in Million €)')
@@ -59,7 +83,7 @@ st.plotly_chart(fig)
 # Top 5 Earning Clubs in Each League
 st.header("Top 5 Earning Clubs in Each League")
 st.markdown("""
-    The top 5 earning clubs in each league are not just football entities; they are economic models of success. This section examines how their on-field performances in domestic and European competitions translate into robust revenue streams, shaping their stature in the transfer market. It's a cycle of success – sporting triumphs boost a club's brand value, attracting sponsorships and investments, which in turn fund future transfers, completing the financial loop.
+    The top 5 earning clubs in each league, across seasons from 1992/93 to 2021/22, are not just football entities; they are economic models of success. This section examines how their on-field performances in domestic and European competitions translate into robust revenue streams, shaping their stature in the transfer market. It's a cycle of success – sporting triumphs boost a club's brand value, attracting sponsorships and investments, which in turn fund future transfers, completing the financial loop.
 """)
 top_earning_clubs = combined_transfers[combined_transfers['transfer_movement'] == 'out'].groupby(['League Name', 'Club Name'])['Transfer Fee in Millions'].sum().reset_index().sort_values(by=['League Name', 'Transfer Fee in Millions'], ascending=[True, False])
 top_5_earning_clubs_per_league = top_earning_clubs.groupby('League Name').head(5)
@@ -69,7 +93,7 @@ st.plotly_chart(fig)
 # Club-Level Spending Analysis
 st.header("Club-Level Spending Analysis")
 st.markdown("""
-    This analysis offers a glimpse into how club-level spending correlates with success on the pitch. We look at examples of clubs that have mastered the art of building winning teams through judicious spending and those who, despite hefty investments, have yet to see proportional returns. It’s a complex balance of financial acumen and sporting insight, where every euro spent is a bet on future glory.
+    This analysis offers a glimpse into how club-level spending correlates with success on the pitch, drawing data from 1992/93 to 2021/22. We look at examples of clubs that have mastered the art of building winning teams through judicious spending and those who, despite hefty investments, have yet to see proportional returns. It’s a complex balance of financial acumen and sporting insight, where every euro spent is a bet on future glory.
 """)
 top_clubs = combined_transfers.groupby(['League Name', 'Club Name'])['Transfer Fee in Millions'].sum().reset_index().sort_values(by=['League Name', 'Transfer Fee in Millions'], ascending=[True, False])
 top_5_clubs_per_league = top_clubs.groupby('League Name').head(5)
@@ -79,7 +103,7 @@ st.plotly_chart(fig)
 # High-Value Player Transfer Trends
 st.header("High-Value Player Transfer Trends")
 st.markdown("""
-    The high-value transfer segment of the market is a clear indicator of a club’s financial muscle and strategic intent. This analysis also touches upon the impact of external factors such as the COVID-19 pandemic, which has prompted a recalibration of transfer strategies, reflecting the market's resilience and adaptability in face of global economic shifts.
+    The high-value transfer segment of the market, from 1992/93 to 2021/22, is a clear indicator of a club’s financial muscle and strategic intent. This analysis also touches upon the impact of external factors such as the COVID-19 pandemic, which has prompted a recalibration of transfer strategies, reflecting the market's resilience and adaptability in face of global economic shifts.
 """)
 high_value_transfers = combined_transfers[combined_transfers['Transfer Fee in Millions'] > 50].sort_values(by='season')
 fig = px.scatter(high_value_transfers, x='season', y='Transfer Fee in Millions', color='League Name', hover_data=['player_name', 'Club Name'], title='High-Value Player Transfer Trends (Transfers Over 50 Million €)')
@@ -88,7 +112,7 @@ st.plotly_chart(fig)
 # Age vs Transfer Fee Analysis for High-Value Players
 st.header("Age vs Transfer Fee Analysis for High-Value Players")
 st.markdown("""
-    In high-value transfers, the correlation between a player's age and their market value is stark. This segment focuses on how 'peak age' varies across positions and significantly influences transfer fees. Young prospects command high prices for their potential, while experienced players bring immediate prowess, each age bracket carrying its unique appeal and risk.
+    In high-value transfers, from 1992/93 to 2021/22, the correlation between a player's age and their market value is stark. This segment focuses on how 'peak age' varies across positions and significantly influences transfer fees. Young prospects command high prices for their potential, while experienced players bring immediate prowess, each age bracket carrying its unique appeal and risk.
 """)
 fig = px.scatter(high_value_transfers, x='age', y='Transfer Fee in Millions', color='League Name', hover_data=['player_name', 'Club Name'], title='Age vs Transfer Fee for High-Value Players (Transfers Over 50 Million €)')
 st.plotly_chart(fig)
@@ -96,7 +120,7 @@ st.plotly_chart(fig)
 # Spending by Player Position for High-Value Players
 st.header("Spending by Player Position for High-Value Players")
 st.markdown("""
-    This section unravels the economic logic behind the varying transfer fees for different player positions in high-value transfers. Understanding this aspect offers insights into how clubs value skills and roles differently based on market demand, tactical trends, and the rarity of skill sets.
+    This section unravels the economic logic behind the varying transfer fees for different player positions in high-value transfers from 1992/93 to 2021/22. Understanding this aspect offers insights into how clubs value skills and roles differently based on market demand, tactical trends, and the rarity of skill sets.
 """)
 position_order = ['Goalkeeper', 'Defence', 'Right-Back', 'Centre-Back', 'Left-Back', 'Midfield', 'Defensive Midfield', 'Right Midfield', 'Central Midfield', 'Left Midfield', 'Attacking Midfield', 'Attack', 'Right Winger', 'Left Winger', 'Centre-Forward', 'Second Striker']
 fig = px.scatter(high_value_transfers, x='position', y='Transfer Fee in Millions', color='League Name', hover_data=['player_name'], title='Spending by Player Position for High-Value Players (Transfers Over 50 Million €)', category_orders={"position": position_order})
@@ -104,21 +128,29 @@ st.plotly_chart(fig)
 st.markdown("""
     In recent headline-making transfers, players like Neymar and Kylian Mbappé of Paris Saint-Germain (PSG) have dominated the scene. PSG's ability to fund such high-profile signings stems from their substantial financial backing by Qatar Sports Investments. This investment has not only elevated PSG’s status in the transfer market but also underscores the growing influence of affluent ownership in reshaping the financial landscape of European football.
 """)
-# Display the images side by side
+
+# URLs or paths to the images
+neymar_image = 'images/neymar_image.jpg'
+psg_logo = 'images/psg_logo.jpg'
+mbappe_image = 'images/mbappe_image.jpg'
+image_paths = [neymar_image, psg_logo, mbappe_image]
+captions = ['Neymar', 'Paris Saint-Germain', 'Kylian Mbappé']
+
+# Create three columns
 col1, col2, col3 = st.columns(3)
+
+# Display an image in each column
 with col1:
-    st.image(neymar_image, caption='Neymar', width=200)
+    st.image(neymar_image, caption='Neymar')
 
 with col2:
-    st.image(psg_logo, caption='Paris Saint-Germain Logo', width=200)
+    st.image(psg_logo, caption='Paris Saint-Germain Logo')
 
 with col3:
-    st.image(mbappe_image, caption='Kylian Mbappé', width=200)
+    st.image(mbappe_image, caption='Kylian Mbappé')
 
 # Conclusion
 st.header("Conclusion: The Economic Powerhouses of Football")
 st.markdown("""
-    Our journey through the economic underpinnings of European football concludes here, but the story of this financial behemoth continues. Looking ahead, the transfer market is poised to be shaped by emerging trends like new broadcasting deals, the digital transformation of sports, and innovative fan engagement strategies. The interplay of these factors with traditional revenue streams will continue to redefine the economic hierarchy of clubs and leagues, shaping the future narrative of football both on and off the field.
+    Our journey through the economic underpinnings of European football, spanning from the 1992/93 to 2021/22 seasons, concludes here, but the story of this financial behemoth continues. Looking ahead, the transfer market is poised to be shaped by emerging trends like new broadcasting deals, the digital transformation of sports, and innovative fan engagement strategies. The interplay of these factors with traditional revenue streams will continue to redefine the economic hierarchy of clubs and leagues, shaping the future narrative of football both on and off the field.
 """, unsafe_allow_html=True)
-
-# End of Script
